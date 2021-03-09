d8e6aaeb52bacd608a59baba1abbd13e15c70d29
Jonas Ritchi <ritchijon@gmail.com>
2021-03-09 19:26:41 +0200

    Adding configuration to AES, now accepting key (which must be 32 bytes, as it is AES-256) and mode (not implemented as of yet, only CBC supported). Also added constant IV.

--- AES.cs
+++ AES.cs
@@ -1,27 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace AesWpf
 {
+    // Useful resources:
+    // https://www.allkeysgenerator.com/Random/Security-Encryption-Key-Generator.aspx - generate byte specific strings
+    // https://cryptii.com/pipes/base64-to-hex - base64 to HEX
+    // https://www.base64encode.org/ - base64 decoder
+
     public class AES
     {
+        private byte[] IV = Encoding.ASCII.GetBytes("TjWnZq4t7w!z%C*F");
         private readonly AesCryptoServiceProvider aesCryptoServiceProvider;
 
-        public AES()
+        public AES(string key, string mode = "CBC")
         {
+            var keyBytes = Encoding.ASCII.GetBytes(key);
+
+            if (keyBytes.Length != 32)
+            {
+                throw new Exception("Key is not 32 bytes long (256 bits for AES256 required)");
+            }
+
             aesCryptoServiceProvider = new AesCryptoServiceProvider
             {
                 BlockSize = 128,
                 KeySize = 256,
                 Mode = CipherMode.CBC,
                 Padding = PaddingMode.PKCS7,
+                IV = IV,
+                Key = keyBytes,
             };
-
-            // generate random key and IV for testing purposes
-            aesCryptoServiceProvider.GenerateIV();
-            aesCryptoServiceProvider.GenerateKey();
         }
 
         public string Encrypt(string plainText)
